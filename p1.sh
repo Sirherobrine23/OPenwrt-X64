738a26516d99ff2e3d6ae86b51189f2549f9a1c3
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-11-25 17:49:35 -0300

    Update p1.sh

--- p1.sh
+++ p1.sh
@@ -2,4 +2,3 @@
 #!/bin/bash
 
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-echo 'src-git luci https://github.com/openwrt/luci.git' >> feeds.conf.default
