46ce33fdbc8734660bddc4fdedc8ccaa6e9aee2e
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-08 19:14:01 -0300

    Oak

--- p1.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
--- system-autoupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-OPENVERSION=$(wget -qO- https://api.github.com/repos/Sirherobrine23/OPenwrt-X64/releases | grep -o '"tag_name": ".*"' | head -n 1 | sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
-[ -z $OPENVERSION ] && exit 1
-
-# VERSIOSS=$(wget -qO- http://api.github.com/repos/Sirherobrine23/OPenwrt-X64/releases | grep -o '"browser_download_url": ".*"' | grep 'sysupgrade' | grep '.bin' | head -n 1 | sed 's/"//g;s/v//g' | sed 's/browser_download_url: //g')
-
-#sysupgrade -c "https://github.com/Sirherobrine23/WR941NDv3-OpenWRTBuild/releases/download/${OPENVERSION}/"
-exit 0
